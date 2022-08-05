8096d1f8bf8239866a70b75dd97554ad8ed39cf3
喵帕斯 <sq_shang_qi@163.com>
2022-08-05 17:57:02 +0800

    修改跨域验证

---
+++ src/main/java/com/sqmusicplus/config/CORSConfig.java
@@ -0,0 +1,28 @@
+package com.sqmusicplus.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Classname CORSConfig
+ * @Description 跨域设置
+ * @Version 1.0.0
+ * @Date 2022/7/14 17:21
+ * @Created by SQ
+ */
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+
+}
