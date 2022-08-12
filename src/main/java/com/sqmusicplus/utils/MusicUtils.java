57558aff3df789c0b788e5736d5b7d4352390263
喵帕斯 <sq_shang_qi@163.com>
2022-08-12 15:54:23 +0800

    修改下载时无专辑图片问题

--- src/main/java/com/sqmusicplus/utils/MusicUtils.java
+++ src/main/java/com/sqmusicplus/utils/MusicUtils.java
@@ -1,7 +1,6 @@
 package com.sqmusicplus.utils;
 
 import cn.hutool.core.img.ImgUtil;
-import cn.hutool.core.util.URLUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
@@ -11,18 +10,15 @@ import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.*;
 import org.jaudiotagger.tag.datatype.Artwork;
-import org.jaudiotagger.tag.flac.FlacTag;
 import org.jaudiotagger.tag.id3.ID3v1Tag;
 import org.jaudiotagger.tag.id3.ID3v24Tag;
 import ws.schild.jave.EncoderException;
 import ws.schild.jave.MultimediaObject;
 import ws.schild.jave.info.MultimediaInfo;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 /**
  * @Classname MusicUtils
@@ -37,70 +33,71 @@ public class MusicUtils {
 
     /**
      * 获取文件内容
+     *
      * @param file
      * @return
      */
     public static MultimediaInfo getMediaFileInfo(File file) {
         MultimediaObject multimediaObject = new MultimediaObject(file);
         try {
-           return multimediaObject.getInfo();
+            return multimediaObject.getInfo();
         } catch (EncoderException e) {
             log.error("获取文件信息失败：{}", e.getMessage());
-           return null;
+            return null;
         }
     }
 
-    public static MultimediaInfo setMediaFileInfo(File file, String title,String album,String artist,String comment,String lyrics,File image) throws TagException, CannotReadException, InvalidAudioFrameException, ReadOnlyFileException, IOException, CannotWriteException {
+    public static MultimediaInfo setMediaFileInfo(File file, String title, String album, String artist, String comment, String lyrics, File image) throws TagException, CannotReadException, InvalidAudioFrameException, ReadOnlyFileException, IOException, CannotWriteException {
         AudioFile af = AudioFileIO.read(file);
         Tag tag = af.getTag();
-        if ( tag instanceof ID3v1Tag ){
+        if (tag instanceof ID3v1Tag) {
             tag = new ID3v24Tag();
         }
-        tag.setField(FieldKey.TITLE,title.trim());
-        tag.setField(FieldKey.ALBUM,album.trim());
-        tag.setField(FieldKey.ARTIST,artist.trim());
-        tag.setField(FieldKey.COMMENT,comment.trim());
-        if(StringUtils.isNotEmpty(lyrics)){
+        tag.setField(FieldKey.TITLE, title.trim());
+        tag.setField(FieldKey.ALBUM, album.trim());
+        tag.setField(FieldKey.ARTIST, artist.trim());
+        tag.setField(FieldKey.COMMENT, comment.trim());
+        if (StringUtils.isNotEmpty(lyrics)) {
             try {
-                tag.setField(FieldKey.LYRICS,lyrics);
+                tag.setField(FieldKey.LYRICS, lyrics);
             } catch (KeyNotFoundException e) {
                 throw new RuntimeException(e);
             } catch (FieldDataInvalidException e) {
                 throw new RuntimeException(e);
             }
         }
-        if (image!=null&&image.exists()){
+        if (image != null && image.exists()) {
             try {
                 Artwork firstArtwork = tag.getFirstArtwork();
                 firstArtwork.setFromFile(image);
                 tag.setField(firstArtwork);
-            }catch (Exception e){
+            } catch (Exception e) {
                 try {
                     Artwork firstArtwork = Artwork.createArtworkFromFile(image);
                     tag.setField(firstArtwork);
                 } catch (UnsupportedOperationException ex) {
                     tag = new ID3v24Tag();
-                    tag.setField(FieldKey.TITLE,title.trim());
-                    tag.setField(FieldKey.ALBUM,album.trim());
-                    tag.setField(FieldKey.ARTIST,artist.trim());
-                    tag.setField(FieldKey.COMMENT,comment.trim());
+                    tag.setField(FieldKey.TITLE, title.trim());
+                    tag.setField(FieldKey.ALBUM, album.trim());
+                    tag.setField(FieldKey.ARTIST, artist.trim());
+                    tag.setField(FieldKey.COMMENT, comment.trim());
                     Artwork artworkFromFile = Artwork.createArtworkFromFile(image);
                     tag.setField(artworkFromFile);
-                    if(StringUtils.isNotEmpty(lyrics)){
-                        tag.setField(FieldKey.LYRICS,lyrics);
+                    if (StringUtils.isNotEmpty(lyrics)) {
+                        tag.setField(FieldKey.LYRICS, lyrics);
                     }
-                }catch (FieldDataInvalidException fex){
+                } catch (FieldDataInvalidException fex) {
                     BufferedImage bufferedImage = ImageIOUtils.read(image);
                     ImgUtil.write(bufferedImage, image);
                     Artwork firstArtwork = Artwork.createArtworkFromFile(image);
                     tag.setField(firstArtwork);
-                }catch (Exception ale){
-                    tag.setField(FieldKey.TITLE,title.trim());
-                    tag.setField(FieldKey.ALBUM,album.trim());
-                    tag.setField(FieldKey.ARTIST,artist.trim());
-                    tag.setField(FieldKey.COMMENT,comment.trim());
-                    if(StringUtils.isNotEmpty(lyrics)){
-                        tag.setField(FieldKey.LYRICS,lyrics);
+                } catch (Exception ale) {
+                    tag.setField(FieldKey.TITLE, title.trim());
+                    tag.setField(FieldKey.ALBUM, album.trim());
+                    tag.setField(FieldKey.ARTIST, artist.trim());
+                    tag.setField(FieldKey.COMMENT, comment.trim());
+                    if (StringUtils.isNotEmpty(lyrics)) {
+                        tag.setField(FieldKey.LYRICS, lyrics);
                     }
                 }
             }
