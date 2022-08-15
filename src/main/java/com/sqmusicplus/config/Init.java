f2746632e8105f1c30b76dfd5a9bea6b670060a5
喵帕斯 <sq_shang_qi@163.com>
2022-08-15 14:11:18 +0800

    增加启动说明

--- src/main/java/com/sqmusicplus/config/Init.java
+++ src/main/java/com/sqmusicplus/config/Init.java
@@ -2,7 +2,9 @@ package com.sqmusicplus.config;
 
 import com.sqmusicplus.entity.DownloadEntity;
 import com.sqmusicplus.utils.EhCacheUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +18,26 @@ import java.util.List;
  * @Date 2022/8/3 16:52
  * @Created by SQ
  */
+@Slf4j
 @Configuration
 public class Init implements ApplicationRunner {
     @Autowired
     MusicConfig musicConfig;
     @Autowired
     DownloadTask downloadTask;
+    @Value("${server.port}")
+    private String port;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        log.info(musicConfig.toString());
+        log.info("启动完毕：http://localhost:{}", port);
         List<Object> values = EhCacheUtil.values(EhCacheUtil.RUN_DOWNLOAD);
         for (Object value : values) {
             DownloadEntity downloadEntity = (DownloadEntity) value;
             try {
-                EhCacheUtil.remove(EhCacheUtil.RUN_DOWNLOAD,downloadEntity.getMusicid());
-                EhCacheUtil.put(EhCacheUtil.READY_DOWNLOAD,downloadEntity.getMusicid(),downloadEntity);
+                EhCacheUtil.remove(EhCacheUtil.RUN_DOWNLOAD, downloadEntity.getMusicid());
+                EhCacheUtil.put(EhCacheUtil.READY_DOWNLOAD, downloadEntity.getMusicid(), downloadEntity);
             } catch (Exception e) {
 
             }
